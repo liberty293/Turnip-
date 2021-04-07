e366f586a21d0e0a2436064feca8bdd0e24cb7d4
tkitts <tyler.kitts@gmail.com>
2021-04-06 21:17:38 -0400

    Eat is disabled after full satisfaction
    I added some unfinished code related to animations and made satisfaction drop to 0 after it maxes out(since the turnip dies)

---
+++ Assets/Code/SatisfactionCode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatisfactionCode : MonoBehaviour
+{
+    public UnityEngine.UI.Slider slide;
+    public Animator turnipAnimator;
+    private bool chomped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        chomped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void IncreaseSatisfaction(float num)
+    {
+        if (chomped == false) {
+            slide.value += num;
+        }
+        if (slide.value == 1)
+        {
+            chomped = true;
+            Consume();
+        }
+    }
+    private void Consume()
+    {
+        slide.value = 0;
+        //turnipAnimator.SetTrigger(getEaten);
+    }
+}
