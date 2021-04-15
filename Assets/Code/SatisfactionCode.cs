907538cf9470e0420a2832402913454091f2bdd2
tkitts <tyler.kitts@gmail.com>
2021-04-15 18:50:04 -0400

    Made satisfaction gain gradual

--- Assets/Code/SatisfactionCode.cs
+++ Assets/Code/SatisfactionCode.cs
@@ -5,29 +5,43 @@ using UnityEngine;
 public class SatisfactionCode : MonoBehaviour
 {
     public UnityEngine.UI.Slider slide;
-    public Animator turnipAnimator;
+    public GameObject turnip;
     private bool chomped;
+    private IEnumerator slowlySat;
+    private bool increasing;
     // Start is called before the first frame update
     void Start()
     {
         chomped = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Feed(float amount)
     {
-        
+
+        slowlySat = SlowlyIncSat(amount, 3);//replace with animation duration
+        StartCoroutine(slowlySat);
     }
-    public void IncreaseSatisfaction(float num)
+    public void Water(float amount)
     {
-        if (chomped == false) {
-            slide.value += num;
+
+        slowlySat = SlowlyIncSat(amount, 7);
+        StartCoroutine(slowlySat);
+    }
+    private IEnumerator SlowlyIncSat(float amount, float time)
+    {
+        if (!increasing) {
+            increasing = true;
+            float timeElapsed = 0;
+            while (timeElapsed < time)
+            {
+                timeElapsed = Mathf.Clamp(timeElapsed + Time.deltaTime, 0, time);
+                slide.value += Time.deltaTime * amount / time;
+                turnip.transform.localScale.Scale(new Vector3(1+Time.deltaTime,1+ Time.deltaTime, 1+ Time.deltaTime));
+                yield return null;
+            }
+            increasing = false;
         }
-        if (slide.value == 1)
-        {
-            chomped = true;
-            Consume();
-        }
+
     }
     private void Consume()
     {
