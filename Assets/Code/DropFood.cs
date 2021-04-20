35b4ab692f65274318bd0281af2527c1f9bd5843
tkitts <tyler.kitts@gmail.com>
2021-04-20 17:48:04 -0400

    Drop Food physics animation

--- Assets/Code/DropFood.cs
+++ Assets/Code/DropFood.cs
@@ -7,8 +7,15 @@ public class DropFood : MonoBehaviour
     public GameObject leaf1;
     public GameObject leaf2;
     public GameObject leaf3;
-    public GameObject plate;
     public void Drop()
+    {
+        leaf1.GetComponent<Rigidbody>().useGravity = true;
+        leaf2.GetComponent<Rigidbody>().useGravity = true;
+        leaf3.GetComponent<Rigidbody>().useGravity = true;
+        this.GetComponent<Rigidbody>().useGravity = true;
+        //then play animation
+    }
+    public void Reset()
     {
 
     }
